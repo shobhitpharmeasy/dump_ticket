90a7bfaaa01666b23964147c1fae058e8df5016f
shobhitpharmeasy <shobhit.aggarwal@pharmeasy.in>
2025-12-18 17:50:45 +0530

    added consumer 1st 2nd - 2nd

---
+++ consumer/1-2/Tickets19_timestamps_update_20251218_174049.sql
@@ -0,0 +1,558 @@
+-- SQL queries to update HD Ticket and Comment timestamps
+-- This script updates creation and modified dates to match Freshdesk data
+-- Generated: 2025-12-18 17:40:49.302921
+-- Total tickets to process: 296
+
+
+-- ========================================
+-- Ticket: HD Ticket 33401 | Freshdesk ID: 22245630
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 20:50:26',
+    modified = '2025-12-02 20:52:23',
+    opening_date = '2025-12-02',
+    opening_time = '20:50:26',
+    resolution_by = 2025-12-04 20:50:26,
+    first_response_time = NULL,
+    resolution_time = NULL
+WHERE name = '33401';
+
+-- Comments for HD Ticket 33401 (Freshdesk ID: 22245630)
+-- Comment mdmcvqf8sn (FD Comment ID: 43974928748)
+UPDATE `tabComment`
+SET creation = '2025-12-02 20:50:26.000000',
+    modified = '2025-12-02 20:50:26.000000'
+WHERE name = 'mdmcvqf8sn';
+
+-- Comment mdqs9n8trj (FD Comment ID: 43974929453)
+UPDATE `tabComment`
+SET creation = '2025-12-02 20:52:23.000000',
+    modified = '2025-12-02 20:52:23.000000'
+WHERE name = 'mdqs9n8trj';
+
+
+
+-- ========================================
+-- Ticket: HD Ticket 33402 | Freshdesk ID: 22245643
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 20:54:20',
+    modified = '2025-12-10 17:53:51',
+    opening_date = '2025-12-02',
+    opening_time = '20:54:20',
+    resolution_by = 2025-12-04 20:54:20,
+    first_response_time = 340,
+    resolution_time = NULL
+WHERE name = '33402';
+
+-- Comments for HD Ticket 33402 (Freshdesk ID: 22245643)
+-- Comment ml0lef60ab (FD Comment ID: 43974934832)
+UPDATE `tabComment`
+SET creation = '2025-12-02 21:08:09.000000',
+    modified = '2025-12-02 21:08:09.000000'
+WHERE name = 'ml0lef60ab';
+
+-- Comment ml3pt5uhgn (FD Comment ID: 43974934890)
+UPDATE `tabComment`
+SET creation = '2025-12-02 21:08:18.000000',
+    modified = '2025-12-02 21:08:18.000000'
+WHERE name = 'ml3pt5uhgn';
+
+-- Comment ml676dm2jh (FD Comment ID: 43975094579)
+UPDATE `tabComment`
+SET creation = '2025-12-03 10:55:08.000000',
+    modified = '2025-12-03 10:55:08.000000'
+WHERE name = 'ml676dm2jh';
+
+-- Comment ml9k6ce7u4 (FD Comment ID: 43975094581)
+UPDATE `tabComment`
+SET creation = '2025-12-03 10:55:08.000000',
+    modified = '2025-12-03 10:55:08.000000'
+WHERE name = 'ml9k6ce7u4';
+
+-- Comment mlcg5bqfjd (FD Comment ID: 43975094889)
+UPDATE `tabComment`
+SET creation = '2025-12-03 10:57:18.000000',
+    modified = '2025-12-03 10:57:18.000000'
+WHERE name = 'mlcg5bqfjd';
+
+-- Comment mlfkm6n9is (FD Comment ID: 43975095251)
+UPDATE `tabComment`
+SET creation = '2025-12-03 10:59:08.000000',
+    modified = '2025-12-03 10:59:08.000000'
+WHERE name = 'mlfkm6n9is';
+
+-- Comment mljofpvj1m (FD Comment ID: 43975998711)
+UPDATE `tabComment`
+SET creation = '2025-12-06 19:41:28.000000',
+    modified = '2025-12-06 19:41:28.000000'
+WHERE name = 'mljofpvj1m';
+
+-- Comment mlmep8pgpq (FD Comment ID: 43976034769)
+UPDATE `tabComment`
+SET creation = '2025-12-07 08:54:37.000000',
+    modified = '2025-12-07 08:54:37.000000'
+WHERE name = 'mlmep8pgpq';
+
+-- Comment mlpvbnbo6f (FD Comment ID: 43976040520)
+UPDATE `tabComment`
+SET creation = '2025-12-07 11:03:43.000000',
+    modified = '2025-12-07 11:03:43.000000'
+WHERE name = 'mlpvbnbo6f';
+
+-- Comment mlsplevjk3 (FD Comment ID: 43976060508)
+UPDATE `tabComment`
+SET creation = '2025-12-07 16:01:16.000000',
+    modified = '2025-12-07 16:01:16.000000'
+WHERE name = 'mlsplevjk3';
+
+-- Comment mlv1nojq4j (FD Comment ID: 43976172499)
+UPDATE `tabComment`
+SET creation = '2025-12-08 15:01:03.000000',
+    modified = '2025-12-08 15:01:03.000000'
+WHERE name = 'mlv1nojq4j';
+
+-- Comment mm2qi7hv2p (FD Comment ID: 43976383087)
+UPDATE `tabComment`
+SET creation = '2025-12-09 08:19:38.000000',
+    modified = '2025-12-09 08:19:38.000000'
+WHERE name = 'mm2qi7hv2p';
+
+-- Comment mm5nk512k8 (FD Comment ID: 43976462891)
+UPDATE `tabComment`
+SET creation = '2025-12-09 16:24:41.000000',
+    modified = '2025-12-09 16:24:41.000000'
+WHERE name = 'mm5nk512k8';
+
+
+
+-- ========================================
+-- Ticket: HD Ticket 33404 | Freshdesk ID: 22245689
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 21:08:10',
+    modified = '2025-12-02 21:08:11',
+    opening_date = '2025-12-02',
+    opening_time = '21:08:10',
+    resolution_by = 2025-12-04 21:08:11,
+    first_response_time = NULL,
+    resolution_time = NULL
+WHERE name = '33404';
+
+
+-- ========================================
+-- Ticket: HD Ticket 33408 | Freshdesk ID: 22245699
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 21:11:03',
+    modified = '2025-12-04 23:20:44',
+    opening_date = '2025-12-02',
+    opening_time = '21:11:03',
+    resolution_by = 2025-12-04 21:11:03,
+    first_response_time = NULL,
+    resolution_time = NULL
+WHERE name = '33408';
+
+-- Comments for HD Ticket 33408 (Freshdesk ID: 22245699)
+-- Comment nbn91eg5t8 (FD Comment ID: 43974935810)
+UPDATE `tabComment`
+SET creation = '2025-12-02 21:11:03.000000',
+    modified = '2025-12-02 21:11:03.000000'
+WHERE name = 'nbn91eg5t8';
+
+-- Comment nbrlq2ke0o (FD Comment ID: 43974936617)
+UPDATE `tabComment`
+SET creation = '2025-12-02 21:13:46.000000',
+    modified = '2025-12-02 21:13:46.000000'
+WHERE name = 'nbrlq2ke0o';
+
+
+
+-- ========================================
+-- Ticket: HD Ticket 33412 | Freshdesk ID: 22245701
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 21:11:18',
+    modified = '2025-12-04 23:20:44',
+    opening_date = '2025-12-02',
+    opening_time = '21:11:18',
+    resolution_by = 2025-12-04 21:11:18,
+    first_response_time = NULL,
+    resolution_time = NULL
+WHERE name = '33412';
+
+-- Comments for HD Ticket 33412 (Freshdesk ID: 22245701)
+-- Comment nip212o7pn (FD Comment ID: 43974935871)
+UPDATE `tabComment`
+SET creation = '2025-12-02 21:11:18.000000',
+    modified = '2025-12-02 21:11:18.000000'
+WHERE name = 'nip212o7pn';
+
+-- Comment nkgitvesu7 (FD Comment ID: 43974935900)
+UPDATE `tabComment`
+SET creation = '2025-12-02 21:11:23.000000',
+    modified = '2025-12-02 21:11:23.000000'
+WHERE name = 'nkgitvesu7';
+
+
+
+-- ========================================
+-- Ticket: HD Ticket 33414 | Freshdesk ID: 22245735
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 21:20:51',
+    modified = '2025-12-04 23:36:49',
+    opening_date = '2025-12-02',
+    opening_time = '21:20:51',
+    resolution_by = 2025-12-04 21:20:51,
+    first_response_time = NULL,
+    resolution_time = NULL
+WHERE name = '33414';
+
+-- Comments for HD Ticket 33414 (Freshdesk ID: 22245735)
+-- Comment nr3gn9id4d (FD Comment ID: 43974938978)
+UPDATE `tabComment`
+SET creation = '2025-12-02 21:20:51.000000',
+    modified = '2025-12-02 21:20:51.000000'
+WHERE name = 'nr3gn9id4d';
+
+-- Comment nr69j6vanc (FD Comment ID: 43974939006)
+UPDATE `tabComment`
+SET creation = '2025-12-02 21:20:57.000000',
+    modified = '2025-12-02 21:20:57.000000'
+WHERE name = 'nr69j6vanc';
+
+
+
+-- ========================================
+-- Ticket: HD Ticket 33415 | Freshdesk ID: 22245819
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 21:51:21',
+    modified = '2025-12-05 00:06:03',
+    opening_date = '2025-12-02',
+    opening_time = '21:51:21',
+    resolution_by = 2025-12-04 21:51:21,
+    first_response_time = NULL,
+    resolution_time = NULL
+WHERE name = '33415';
+
+-- Comments for HD Ticket 33415 (Freshdesk ID: 22245819)
+-- Comment o0pmtjhqkr (FD Comment ID: 43974948899)
+UPDATE `tabComment`
+SET creation = '2025-12-02 21:51:21.000000',
+    modified = '2025-12-02 21:51:21.000000'
+WHERE name = 'o0pmtjhqkr';
+
+-- Comment o0sd8l82fg (FD Comment ID: 43974948925)
+UPDATE `tabComment`
+SET creation = '2025-12-02 21:51:25.000000',
+    modified = '2025-12-02 21:51:25.000000'
+WHERE name = 'o0sd8l82fg';
+
+
+
+-- ========================================
+-- Ticket: HD Ticket 33416 | Freshdesk ID: 22245844
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 22:07:05',
+    modified = '2025-12-05 00:21:01',
+    opening_date = '2025-12-02',
+    opening_time = '22:07:05',
+    resolution_by = 2025-12-04 22:07:05,
+    first_response_time = NULL,
+    resolution_time = NULL
+WHERE name = '33416';
+
+-- Comments for HD Ticket 33416 (Freshdesk ID: 22245844)
+-- Comment o6v5bnq328 (FD Comment ID: 43974953977)
+UPDATE `tabComment`
+SET creation = '2025-12-02 22:07:05.000000',
+    modified = '2025-12-02 22:07:05.000000'
+WHERE name = 'o6v5bnq328';
+
+-- Comment o728c0hs1d (FD Comment ID: 43974953987)
+UPDATE `tabComment`
+SET creation = '2025-12-02 22:07:07.000000',
+    modified = '2025-12-02 22:07:07.000000'
+WHERE name = 'o728c0hs1d';
+
+
+
+-- ========================================
+-- Ticket: HD Ticket 33419 | Freshdesk ID: 22245846
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 22:07:41',
+    modified = '2025-12-05 00:21:01',
+    opening_date = '2025-12-02',
+    opening_time = '22:07:41',
+    resolution_by = 2025-12-04 22:07:41,
+    first_response_time = NULL,
+    resolution_time = NULL
+WHERE name = '33419';
+
+-- Comments for HD Ticket 33419 (Freshdesk ID: 22245846)
+-- Comment od27rp3asn (FD Comment ID: 43974954207)
+UPDATE `tabComment`
+SET creation = '2025-12-02 22:07:41.000000',
+    modified = '2025-12-02 22:07:41.000000'
+WHERE name = 'od27rp3asn';
+
+-- Comment od642ip51o (FD Comment ID: 43974954236)
+UPDATE `tabComment`
+SET creation = '2025-12-02 22:07:45.000000',
+    modified = '2025-12-02 22:07:45.000000'
+WHERE name = 'od642ip51o';
+
+
+
+-- ========================================
+-- Ticket: HD Ticket 33421 | Freshdesk ID: 22245853
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 22:18:42',
+    modified = '2025-12-05 00:21:01',
+    opening_date = '2025-12-02',
+    opening_time = '22:18:42',
+    resolution_by = 2025-12-04 22:18:42,
+    first_response_time = NULL,
+    resolution_time = NULL
+WHERE name = '33421';
+
+-- Comments for HD Ticket 33421 (Freshdesk ID: 22245853)
+-- Comment ojqa21f18k (FD Comment ID: 43974957715)
+UPDATE `tabComment`
+SET creation = '2025-12-02 22:18:42.000000',
+    modified = '2025-12-02 22:18:42.000000'
+WHERE name = 'ojqa21f18k';
+
+-- Comment oju77s2i79 (FD Comment ID: 43974957740)
+UPDATE `tabComment`
+SET creation = '2025-12-02 22:18:46.000000',
+    modified = '2025-12-02 22:18:46.000000'
+WHERE name = 'oju77s2i79';
+
+
+
+-- ========================================
+-- Ticket: HD Ticket 33423 | Freshdesk ID: 22245862
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 22:38:36',
+    modified = '2025-12-05 00:50:41',
+    opening_date = '2025-12-02',
+    opening_time = '22:38:36',
+    resolution_by = 2025-12-04 22:38:36,
+    first_response_time = NULL,
+    resolution_time = NULL
+WHERE name = '33423';
+
+-- Comments for HD Ticket 33423 (Freshdesk ID: 22245862)
+-- Comment or61g1t491 (FD Comment ID: 43974963841)
+UPDATE `tabComment`
+SET creation = '2025-12-02 22:38:36.000000',
+    modified = '2025-12-02 22:38:36.000000'
+WHERE name = 'or61g1t491';
+
+-- Comment orau70rg59 (FD Comment ID: 43974963865)
+UPDATE `tabComment`
+SET creation = '2025-12-02 22:38:41.000000',
+    modified = '2025-12-02 22:38:41.000000'
+WHERE name = 'orau70rg59';
+
+
+
+-- ========================================
+-- Ticket: HD Ticket 33426 | Freshdesk ID: 22245865
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 22:41:03',
+    modified = '2025-12-05 00:50:41',
+    opening_date = '2025-12-02',
+    opening_time = '22:41:03',
+    resolution_by = 2025-12-04 22:41:03,
+    first_response_time = NULL,
+    resolution_time = NULL
+WHERE name = '33426';
+
+-- Comments for HD Ticket 33426 (Freshdesk ID: 22245865)
+-- Comment p1umrj3a73 (FD Comment ID: 43974964489)
+UPDATE `tabComment`
+SET creation = '2025-12-02 22:41:03.000000',
+    modified = '2025-12-02 22:41:03.000000'
+WHERE name = 'p1umrj3a73';
+
+-- Comment p21hkp7ugh (FD Comment ID: 43974964510)
+UPDATE `tabComment`
+SET creation = '2025-12-02 22:41:07.000000',
+    modified = '2025-12-02 22:41:07.000000'
+WHERE name = 'p21hkp7ugh';
+
+
+
+-- ========================================
+-- Ticket: HD Ticket 33429 | Freshdesk ID: 22245884
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 22:58:08',
+    modified = '2025-12-05 01:06:28',
+    opening_date = '2025-12-02',
+    opening_time = '22:58:08',
+    resolution_by = 2025-12-04 22:58:09,
+    first_response_time = NULL,
+    resolution_time = NULL
+WHERE name = '33429';
+
+-- Comments for HD Ticket 33429 (Freshdesk ID: 22245884)
+-- Comment p8ps76plo5 (FD Comment ID: 43974969291)
+UPDATE `tabComment`
+SET creation = '2025-12-02 22:58:09.000000',
+    modified = '2025-12-02 22:58:09.000000'
+WHERE name = 'p8ps76plo5';
+
+-- Comment pat3ut27cg (FD Comment ID: 43974969313)
+UPDATE `tabComment`
+SET creation = '2025-12-02 22:58:13.000000',
+    modified = '2025-12-02 22:58:13.000000'
+WHERE name = 'pat3ut27cg';
+
+-- Comment pb1ov80eus (FD Comment ID: 43975093188)
+UPDATE `tabComment`
+SET creation = '2025-12-03 10:45:35.000000',
+    modified = '2025-12-03 10:45:35.000000'
+WHERE name = 'pb1ov80eus';
+
+
+
+-- ========================================
+-- Ticket: HD Ticket 33436 | Freshdesk ID: 22245901
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 23:15:02',
+    modified = '2025-12-05 01:23:28',
+    opening_date = '2025-12-02',
+    opening_time = '23:15:02',
+    resolution_by = 2025-12-04 23:15:02,
+    first_response_time = NULL,
+    resolution_time = NULL
+WHERE name = '33436';
+
+-- Comments for HD Ticket 33436 (Freshdesk ID: 22245901)
+-- Comment pi4hvkritg (FD Comment ID: 43974973726)
+UPDATE `tabComment`
+SET creation = '2025-12-02 23:15:02.000000',
+    modified = '2025-12-02 23:15:02.000000'
+WHERE name = 'pi4hvkritg';
+
+-- Comment pi739dh082 (FD Comment ID: 43974973748)
+UPDATE `tabComment`
+SET creation = '2025-12-02 23:15:07.000000',
+    modified = '2025-12-02 23:15:07.000000'
+WHERE name = 'pi739dh082';
+
+-- Comment piblb7v30n (FD Comment ID: 43975090919)
+UPDATE `tabComment`
+SET creation = '2025-12-03 10:29:19.000000',
+    modified = '2025-12-03 10:29:19.000000'
+WHERE name = 'piblb7v30n';
+
+
+-- ========================================
+-- Ticket: HD Ticket 33388 | Freshdesk ID: 22245762
+-- ========================================
+UPDATE `tabHD Ticket`
+SET creation = '2025-12-02 21:31:51',
+    modified = '2025-12-17 13:55:22',
+    opening_date = '2025-12-02',
+    opening_time = '21:31:51',
+    resolution_by = 2025-12-04 21:31:51,
+    first_response_time = 622522,
+    resolution_time = NULL
+WHERE name = '33388';
+
+-- Comments for HD Ticket 33388 (Freshdesk ID: 22245762)
+-- Comment kgsjlkqiit (FD Comment ID: 43974942663)
+UPDATE `tabComment`
+SET creation = '2025-12-02 21:31:51.000000',
+    modified = '2025-12-02 21:31:51.000000'
+WHERE name = 'kgsjlkqiit';
+
+-- Comment kh0ou9j3gq (FD Comment ID: 43974942681)
+UPDATE `tabComment`
+SET creation = '2025-12-02 21:31:55.000000',
+    modified = '2025-12-02 21:31:55.000000'
+WHERE name = 'kh0ou9j3gq';
+
+-- Comment kh38bb9a7m (FD Comment ID: 43974946189)
+UPDATE `tabComment`
+SET creation = '2025-12-02 21:42:50.000000',
+    modified = '2025-12-02 21:42:50.000000'
+WHERE name = 'kh38bb9a7m';
+
+-- Comment kh513tatci (FD Comment ID: 43975109592)
+UPDATE `tabComment`
+SET creation = '2025-12-03 12:24:19.000000',
+    modified = '2025-12-03 12:24:19.000000'
+WHERE name = 'kh513tatci';
+
+-- Comment kh8kgnejr6 (FD Comment ID: 43975172902)
+UPDATE `tabComment`
+SET creation = '2025-12-03 16:44:37.000000',
+    modified = '2025-12-03 16:44:37.000000'
+WHERE name = 'kh8kgnejr6';
+
+-- Comment khb7p9s644 (FD Comment ID: 43975481239)
+UPDATE `tabComment`
+SET creation = '2025-12-04 16:18:10.000000',
+    modified = '2025-12-04 16:18:10.000000'
+WHERE name = 'khb7p9s644';
+
+-- Comment kheqbdj6qv (FD Comment ID: 43975627826)
+UPDATE `tabComment`
+SET creation = '2025-12-05 00:53:53.000000',
+    modified = '2025-12-05 00:53:53.000000'
+WHERE name = 'kheqbdj6qv';
+
+-- Comment khhbj8n56r (FD Comment ID: 43975781031)
+UPDATE `tabComment`
+SET creation = '2025-12-05 16:41:34.000000',
+    modified = '2025-12-05 16:41:34.000000'
+WHERE name = 'khhbj8n56r';
+
+-- Comment khm80roof4 (FD Comment ID: 43975781396)
+UPDATE `tabComment`
+SET creation = '2025-12-05 16:43:00.000000',
+    modified = '2025-12-05 16:43:00.000000'
+WHERE name = 'khm80roof4';
+
+-- Comment khr7pci35e (FD Comment ID: 43975964736)
+UPDATE `tabComment`
+SET creation = '2025-12-06 11:54:52.000000',
+    modified = '2025-12-06 11:54:52.000000'
+WHERE name = 'khr7pci35e';
+
+-- Comment khus3s2k4a (FD Comment ID: 43977018909)
+UPDATE `tabComment`
+SET creation = '2025-12-11 16:55:04.000000',
+    modified = '2025-12-11 16:55:04.000000'
+WHERE name = 'khus3s2k4a';
+
+-- Comment ki22vtpq14 (FD Comment ID: 43977929308)
+UPDATE `tabComment`
+SET creation = '2025-12-16 13:12:12.000000',
+    modified = '2025-12-16 13:12:12.000000'
+WHERE name = 'ki22vtpq14';
+
+-- Comment ki7kg6ugh1 (FD Comment ID: 43978212859)
+UPDATE `tabComment`
+SET creation = '2025-12-17 13:52:35.000000',
+    modified = '2025-12-17 13:52:35.000000'
+WHERE name = 'ki7kg6ugh1';
+
+-- Comment kiefdgnr46 (FD Comment ID: 43978213374)
+UPDATE `tabComment`
+SET creation = '2025-12-17 13:55:22.000000',
+    modified = '2025-12-17 13:55:22.000000'
+WHERE name = 'kiefdgnr46';
